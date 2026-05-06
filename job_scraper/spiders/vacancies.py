d3ad9da48d50af80ebf1c03282e50c9daa13c46c
Valerii <valerapiliaev@gmail.com>
2026-05-06 23:37:21 +0300

    feat: add scraper for job vacancies

---
+++ job_scraper/spiders/vacancies.py
@@ -0,0 +1,35 @@
+import scrapy
+from scrapy.http import Response
+from typing import Generator, Dict, Any
+
+
+class VacanciesSpider(scrapy.Spider):
+    name = "vacancies"
+    allowed_domains = ["work.ua"]
+    start_urls = ["https://www.work.ua/jobs-python/"]
+
+    def parse(self, response: Response) -> Generator:
+        job_cards = response.css("div.card-hover") or response.css("div.card")
+        for job in job_cards:
+            link = job.css("h2 a::attr(href)").get()
+            if link:
+                yield response.follow(link, callback=self.parse_job)
+        next_page = response.css("ul.pagination li.active + li a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_job(
+            self,
+            response: Response
+    ) -> Generator[Dict[str, Any], None, None]:
+        title = response.css("h1::text").get()
+        description = " ".join(
+            response.css("div#job-description *::text").getall()
+        ).strip()
+        company = response.css("div.company-info a::text").get()
+        yield {
+            "title": title,
+            "company": company,
+            "description": description,
+            "url": response.url,
+        }
